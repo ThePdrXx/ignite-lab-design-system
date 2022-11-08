d5587414f7c453239ef8bb690d8f45b7ea6fd5ce
ThePdrXx <pdrpvp0310@gmail.com>
2022-11-07 22:23:16 -0300

    After

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import './styles/global.css'
+import { SignIn } from "./pages/SignIn";
+
+import "./styles/global.css";
 
 export function App() {
   return (
-    <h1 className='text-cyan-500'>Hello World</h1>
+   <SignIn />
   )
 }
-
